dcfea646f4e4871313855e77008f3f8b441887e4
CeritNorts <20223l001058@utcv.edu.mx>
2025-03-18 09:39:47 -0600

    Recuperacion del usuario para el perfil

--- src/app/components/comentarios/comentarios.component.ts
+++ src/app/components/comentarios/comentarios.component.ts
@@ -1,17 +1,6 @@
 // comentarios.component.ts
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { Emoji } from 'emoji-mart';
-interface Comentario {
-  id: number;
-  usuario: string;
-  texto: string;
-  likes: number;
-  respuestas?: Comentario[];
-  showReplyBox?: boolean;
-}
 
 @Component({
   selector: 'app-comentarios',
@@ -21,48 +10,5 @@ interface Comentario {
 
 })
 export class ComentariosComponent {
-  comentarios: Comentario[] = [];
-  nuevoComentario: string = '';
-  mostrarEmojiPicker = false; 
-
-  agregarComentario() {
-    if (this.nuevoComentario.trim()) {
-      this.comentarios.push({
-        id: Date.now(),
-        usuario: 'Usuario',
-        texto: this.nuevoComentario,
-        likes: 0,
-        respuestas: []
-      });
-      this.nuevoComentario = '';
-    }
-  }
-
-  darLike(comentario: Comentario) {
-    comentario.likes++;
-  }
-
-  responderComentario(comentario: Comentario, respuestaTexto: string) {
-    if (respuestaTexto.trim()) {
-      comentario.respuestas?.push({
-        id: Date.now(),
-        usuario: 'Usuario',
-        texto: respuestaTexto,
-        likes: 0,
-      });
-    }
-  }
-
-  toggleReplyBox(comentario: Comentario) {
-    comentario.showReplyBox = !comentario.showReplyBox;
-  }
-
-  agregarEmoji(event: any) {
-    this.nuevoComentario += event.emoji.native; // Agrega el emoji al comentario
-  }
-
-  // Método para mostrar/ocultar el picker de emojis
-  toggleEmojiPicker() {
-    this.mostrarEmojiPicker = !this.mostrarEmojiPicker; // Cambiar la propiedad para mostrar/ocultar el emoji picker
-  }
+  
 }
