4b9e5015e2a6311a08e5e3740e35acff603576f4
v4r13d3s <20233L001041@utcv.edu.mx>
2025-03-18 00:47:51 -0600

    Actualización, subo mi search actualizado

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface MediaItem {
+  type: 'image' | 'video';
+  src: string;
+  thumbnail?: string;
+  caption?: string;
+}
 
 @Component({
   selector: 'app-tab2',
@@ -8,6 +15,32 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  mediaItems: MediaItem[] = [];
+
   constructor() {}
 
+  
+  ngOnInit() {
+    this.mediaItems = [
+      { type: 'image', src: 'assets/img/Luffy.jpg', caption: 'oh oh oh' },
+      { type: 'image', src: 'assets/img/niki.jpg', caption: 'Fun Times' },
+      { type: 'image', src: 'assets/img/chica.jpg', caption: 'Stylish' },
+      { type: 'image', src: 'assets/img/flores-f.jpg', caption: 'Cool Vibes' }, // Nueva fila 1
+      { type: 'image', src: 'assets/img/perro.jpg', caption: 'Adventure Time' },
+      { type: 'image', src: 'assets/img/gato.jpg', caption: 'Chilling' },
+      { type: 'image', src: 'assets/img/chi.jpg', caption: 'Highlights' }, // Nueva fila 2
+      { type: 'image', src: 'assets/img/elrubius.jpg', caption: 'Epic Views' },
+      { type: 'image', src: 'assets/img/cora.jpg', caption: 'Moments' },
+      { type: 'image', src: 'assets/img/totoro.jpg', caption: 'Memories' }, // Nueva fila 3
+      { type: 'image', src: 'assets/img/niki.jpg', caption: 'Fun Times' },
+      { type: 'image', src: 'assets/img/luffy-l.jpg', caption: 'Scenic' },
+      { type: 'image', src: 'assets/img/Luffy.jpg', caption: 'oh oh oh' },
+      { type: 'image', src: 'assets/img/elrubius.jpg', caption: 'Epic Views' },
+      { type: 'image', src: 'assets/img/chica.jpg', caption: 'Stylish' },
+      { type: 'image', src: 'assets/img/Luffy.jpg', caption: 'oh oh oh' },
+      { type: 'image', src: 'assets/img/cora.jpg', caption: 'Moments' },
+      { type: 'image', src: 'assets/img/chica.jpg', caption: 'Stylish' },
+    ];
+  }
+  
 }
